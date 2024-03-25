6e9164954e56bf37b48838d546fda05fb1533501
Hemant Singh <codingmydream002@gmail.com>
2024-03-25 14:37:13 +0530

    completed v1

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 import express from "express";
 var app = express();
 
+
+import path from "path";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
-var cors = require('cors');
+import cors from "cors";
 app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
 
 app.use(express.static('public'));
@@ -15,8 +21,33 @@ app.get("/", function (req, res) {
 
 
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.get("/api/:date?", function (req, res) {
+  const new_date = req.params.date;
+  const month = ["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"];
+  const days = ["Sun", "Mon", "Tues", "Wed", "Thr", "Fri", "Sat"];
+  let dt = null;
+
+  if(new_date){
+    if(!isNaN(Date.parse(new_date))){
+      dt = new Date(new_date)
+    } else {
+      if(!isNaN(new Date(Number(new_date)))){
+        dt = new Date(Number(new_date));
+      } else {
+        res.json({error : "Invalid Date"});
+        return;
+      }
+    }
+  } else {
+    dt = new Date();
+  }
+  const uxitTs = dt.getTime();
+  const utcString = `${days[dt.getDay()]}, ${dt.getDate()} ${month[dt.getMonth()]} ${dt.getFullYear()} ${dt.getHours()}:${dt.getMinutes()}:${dt.getSeconds()} GMT`;
+  const data = {
+    unix : uxitTs,
+    utc : utcString
+  };
+  res.json(data);
 });
 
 
